943d0ba491da07cce386b22c835683d06fad5883
Mohammed Youcef <moyoumos@gmail.com>
2024-10-27 15:37:35 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Check if createClient is available
     if (typeof supabase === 'undefined' || typeof supabase.createClient !== 'function') {
-        console.error('Supabase library did not load correctly.'); else console.log('Supabase client');
+        console.error('Supabase library did not load correctly.');
         return;
     }
+}
     
 const supabaseUrl = 'https://gakwgsmogfbwhfuyjckn.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdha3dnc21vZ2Zid2hmdXlqY2tuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwMzQ2MjUsImV4cCI6MjA0NTYxMDYyNX0.z62vWuiYLoDKGrmr3oCx7w0P465p07DzSpApYdt8p4Q';
