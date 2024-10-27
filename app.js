ef6cc249689cc60263b739b74d31bee121dbb958
Mohammed Youcef <moyoumos@gmail.com>
2024-10-27 15:19:38 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-import { createClient } from '@supabase/supabase-js';
-
 const supabaseUrl = 'https://gakwgsmogfbwhfuyjckn.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdha3dnc21vZ2Zid2hmdXlqY2tuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwMzQ2MjUsImV4cCI6MjA0NTYxMDYyNX0.z62vWuiYLoDKGrmr3oCx7w0P465p07DzSpApYdt8p4Q';
 const supabase = createClient(supabaseUrl, supabaseKey);
