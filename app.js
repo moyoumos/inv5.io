6d4fe2894d3a99f581bac0803e69865db1e8f2fc
Mohammed Youcef <moyoumos@gmail.com>
2024-10-27 19:01:44 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Initialize Supabase client
 const supabaseUrl = 'https://gakwgsmogfbwhfuyjckn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdha3dnc21vZ2Zid2hmdXlqY2tuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwMzQ2MjUsImV4cCI6MjA0NTYxMDYyNX0.z62vWuiYLoDKGrmr3oCx7w0P465p07DzSpApYdt8p4Q';
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseKey = 'YOUR_SUPABASE_KEY';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 // Define Alpine.js component
 function itemTable() {
