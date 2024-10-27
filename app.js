85145b216312fbde24da42b37aa9302d0a018ca6
Mohammed Youcef <moyoumos@gmail.com>
2024-10-27 15:22:15 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const supabaseUrl = 'https://gakwgsmogfbwhfuyjckn.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdha3dnc21vZ2Zid2hmdXlqY2tuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwMzQ2MjUsImV4cCI6MjA0NTYxMDYyNX0.z62vWuiYLoDKGrmr3oCx7w0P465p07DzSpApYdt8p4Q';
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 // Get the table body element to populate data
 const tableBody = document.querySelector('#itemsTable tbody');
